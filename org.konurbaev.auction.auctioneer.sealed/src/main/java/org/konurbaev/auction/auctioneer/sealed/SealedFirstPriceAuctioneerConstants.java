ce547a0a293d05f64ee5102ed78a67459429ab22
konurbaev-e <forget>
2015-11-25 21:38:53 +0300

    refactoring

---
+++ org.konurbaev.auction.auctioneer.sealed/src/main/java/org/konurbaev/auction/auctioneer/sealed/SealedFirstPriceAuctioneerConstants.java
@@ -0,0 +1,6 @@
+package org.konurbaev.auction.auctioneer.sealed;
+
+public interface SealedFirstPriceAuctioneerConstants {
+    String SEALED_FIRST_PRICE = "Sealed-First-Price";
+    String DURATION = "3";
+}
